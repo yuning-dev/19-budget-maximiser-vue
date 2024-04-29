b18daadc2baa4ff48d86bb3288834cf1df7622a9
yuning-dev <yuning.eng@gmail.com>
2024-04-29 11:22:25 +0100

    fix mustHave checkbox to reset after Add item button is clicked; add checks for name and cost, as well as error messages

--- budget-maximiser/src/App.vue
+++ budget-maximiser/src/App.vue
@@ -3,32 +3,47 @@
         <h1>Budget maximiser</h1>
         <h2>Give us you budget, we'll get you the best value!</h2>
         <div :class="$style.mainWrapper">
-            <form :class="[$style.budget, $style.box]" action="javascript:void(0);">
-                <div :class="$style.budgetInner">
-                    <label for="budget">Your budget</label><input type="text" id="budget">
-                </div>
-            </form>
-            <div :class="[$style.addItemsWrapper, $style.box]">
-                <p>Add items</p> <br>
-                <form :class="$style.addItemsInnerWrapper" action="javascript:void(0);">
-                    <div :class="$style.formItem">
-                        <label for="itemName">Item name</label> 
-                        <input type="text" id="itemName" v-model="name">
-                    </div>
-                    <div :class="$style.formItem">
-                        <label for="itemCost">Item cost</label> 
-                        <input type="text" id="itemCost" v-model="cost">
+            <div>
+                <form :class="[$style.budget, $style.box]" action="javascript:void(0);">
+                    <div :class="$style.budgetInner">
+                        <label for="budget">Your budget</label><input type="text" id="budget">
                     </div>
                 </form>
-                <div :class="$style.mustHaveWrapper">
-                    <label for="mustHave">Is it a must-have</label><input type="checkbox" id="mustHave" @click="toggleMustHave()" >
+                <div :class="[$style.addItemsWrapper, $style.box]">
+                    <p>Add items</p> <br>
+                    <form :class="$style.addItemsInnerWrapper" action="javascript:void(0);">
+                        <div :class="$style.formItem">
+                            <label for="itemName">Item name</label> 
+                            <input type="text" id="itemName" v-model="name">
+                        </div>
+                        <div :class="$style.formItem">
+                            <label for="itemCost">Item cost</label> 
+                            <input type="text" id="itemCost" v-model="cost">
+                        </div>
+                    </form>
+                    <template v-if="this.addBtnClicked && !validName">
+                        <div :class="$style.errorDiv">
+                            Please enter a valid name.
+                        </div>
+                    </template>
+                    <template v-if="this.addBtnClicked && !validCost">
+                        <div :class="$style.errorDiv">
+                            Please enter a cost that is > 0.
+                        </div>
+                    </template>
+                    <div :class="$style.mustHaveWrapper">
+                        <label for="mustHave">Is it a must-have</label><input type="checkbox" id="mustHave" :checked="isMustHave" @click="toggleMustHave()" >
+                    </div>
+                    <div :class="$style.addBtnWrapper">
+                        <button :class="$style.addItemBtn" @click="addItemBtnClicked">Add item</button>
+                    </div>
                 </div>
-                <button :class="$style.addItemBtn" @click="addItemBtnClicked">Add item</button>
             </div>
             <div :class="$style.itemsList">
-
+                <template v-for="item in items">
+                </template>
             </div>
-            <button :class="$style.generateValueBtn">Generate Max Value!</button>
+            <!-- <button :class="$style.generateValueBtn">Generate Max Value!</button> -->
         </div>
     </div>
 </template>
@@ -38,33 +53,50 @@ export default {
     name: 'App',
     data() {
         return {
-            name: null,
-            cost: null,
+            name: '',
+            cost: '',
             isMustHave: false,
-            items: []
+            items: [],
+            addBtnClicked: false
         }
+    },
+    computed: {
+        validName() {
+            const trimmedName = this.name.trim()
+            const isNumber = Number(trimmedName)
+            if (trimmedName.length > 0 && Number.isNaN(isNumber)) {
+                return true
+            }
+            return false
+        },
+        validCost() {
+            if (!Number.isNaN(this.cost) && this.cost > 0) {
+                return true
+            }
+            return false
+        },
     },
     methods: {
         toggleMustHave() {
-            if (this.isMustHave) {
-                this.isMustHave = false
-                console.log('I should be false')
-            } else {
-                this.isMustHave = true
-                console.log('Hi I am true')
-            }
+            this.isMustHave = !this.isMustHave
         },
         addItemBtnClicked() {
-            const item = {
-                name: this.name,
-                cost: this.cost,
-                mustHave: this.isMustHave
+            this.addBtnClicked = true
+            if (this.validName && this.validCost) {
+                const item = {
+                    name: this.name,
+                    cost: this.cost,
+                    mustHave: this.isMustHave
+                }
+
+                this.items.push(item)
+                this.name = ''
+                this.cost = ''
+                this.isMustHave = false
+                console.log(item)
+                console.log(this.items)
             }
-            this.items.push(item)
-            console.log(item)
-            console.log(this.items)
-            this.name = ''
-            this.cost = ''
+
         }
     }
 }
